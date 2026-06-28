12ec0695b3d2d416768abd69182ea2ad8457a5f4
nman98 <nmanojlovic98@gmail.com>
2026-06-28 20:31:17 +0200

    Fix grammer in mdx_gen

--- mdx_gen/src/config_loader/loader.py
+++ mdx_gen/src/config_loader/loader.py
@@ -30,7 +30,7 @@ class Config:
     validator_amount: str
     path: str
     snapshots: list[dict[str, str]]
-    aditional_info: dict[str, Any] | None = None # to be asumed as empty unless specified
+    additional_info: dict[str, Any] | None = None # to be asumed as empty unless specified
 
 @dataclass
 class ConfigList:
