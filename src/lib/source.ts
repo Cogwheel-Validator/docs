4b1565984b9a7c80d40b4c0c622cf6ab6cf2642e
nman98 <nmanojlovic98@gmail.com>
2026-07-25 12:39:53 +0200

    deps: update fumadocs and nextjs dependencies

--- src/lib/source.ts
+++ src/lib/source.ts
@@ -1,22 +1,22 @@
 import { networks, services, spectraPortal } from "@/.source";
 import { loader } from "fumadocs-core/source";
-import { createMDXSource } from "fumadocs-mdx";
+import { toFumadocsSource } from "fumadocs-mdx/runtime/server";
 
 // See https://fumadocs.vercel.app/docs/headless/source-api for more info
 // Networks documentation source
 export const networksSource = loader({
   baseUrl: "/networks",
-  source: createMDXSource(networks),
+  source: toFumadocsSource(networks, []),
 });
 
 // Services documentation source
 export const servicesSource = loader({
   baseUrl: "/services",
-  source: createMDXSource(services),
+  source: toFumadocsSource(services, []),
 });
 
 // Spectra Portal documentation source
 export const spectraPortalSource = loader({
   baseUrl: "/spectra-portal",
-  source: createMDXSource(spectraPortal),
+  source: toFumadocsSource(spectraPortal, []),
 });
